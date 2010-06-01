667bce2fc2311faf6885af4890c078c9f0382a4a
James Austin <jamesaustin@mac.com>
2010-06-01 10:39:17 +0200

    xrange.

--- 012.py
+++ 012.py
@@ -3,7 +3,7 @@ x = 0
 while True:
     t += x
     divisors = 2 # 1 and itself
-    for y in range(2, int(t ** 0.5)):
+    for y in xrange(2, int(t ** 0.5)):
         if t % y == 0:
             divisors += 2
     if divisors > 500:
